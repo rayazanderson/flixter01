9a212e86ac084de9761770257783a8f6af2c870f
rayazanderson <rayazand@gmail.com>
2019-07-10 03:05:21 +0000

    had to correct my index.html files

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,15 @@
 class CoursesController < ApplicationController
-   def index
+  def index
     @courses = Course.all
-    end
-end
+  end
+
+   def create
+    @course = current_user.courses.create(course_params)
+    redirect_to instructor_course_path(@course)
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:title, :description, :cost)
+  end
