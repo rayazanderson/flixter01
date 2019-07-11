c66062acff93c9982862e50325e71ea45f0beb33
rayazanderson <rayazand@gmail.com>
2019-07-11 05:53:19 +0000

    lesson addons and finalizing params

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :courses, only: [:index, :show]
   namespace :instructor do
-      resources :courses, only: [:new, :create, :show]
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
+    resources :sections, only: []
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:new, :create]
+    end
   end
 end
