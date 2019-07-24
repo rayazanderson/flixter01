5738841126044640a2f1d4ac0351841123e91b0e
rayazanderson <rayazand@gmail.com>
2019-07-24 23:46:00 +0000

    rerouting

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -3,7 +3,7 @@ class Instructor::LessonsController < ApplicationController
   before_action :require_authorized_for_current_section, only: [:new, :create]
   before_action :require_authorized_for_current_lesson, only: [:update]
 
-   def create
+  def create
     @lesson = current_section.lessons.create(lesson_params)
     redirect_to instructor_course_path(current_section.course)
   end
@@ -15,28 +15,24 @@ class Instructor::LessonsController < ApplicationController
 
   private
 
+  def current_lesson
+    @current_lesson ||= Lesson.find(params[:id])
+  end
+
   def require_authorized_for_current_lesson
     if current_lesson.section.course.user != current_user
       render plain: 'Unauthorized', status: :unauthorized
     end
   end
 
-  def current_lesson
-    @current_lesson ||= Lesson.find(params[:id])
-  end
-
-  def require_authorized_for_current_section
-    if current_section.course.user != current_user
-      render plain: 'Unauthorized', status: :unauthorized
-    end
-  end
-
-  helper_method :current_section
+  helper_method :current_lesson
   def current_section
-    @current_section ||= Section.find(params[:section_id])
-  end
+      @current_section ||= Section.find(params[:section_id])
+    end
+
 
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :dashboard, only: [:show]
   root 'static_pages#index'
-  get 'privacy', to: 'static_pages#privacy'
-  get 'career', to: 'static_pages#career'
-  get 'team', to: 'static_pages#team'
+    get 'privacy', to: 'static_pages#privacy'
+    get 'career', to: 'static_pages#career'
+    get 'team', to: 'static_pages#team'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   resources :lessons, only: [:show]
   namespace :instructor do
   resources :lessons, only: [:update]
     resources :sections, only: [:update] do
       resources :lessons, only: [:create]
+        resources :sections, only: [] do
+          resources :lessons, only: [:create]
+      end
     end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create]
