01aa09079e47cc1edaee46bb55ed6362be2ce73f
rayazanderson <rayazand@gmail.com>
2019-07-24 03:53:19 +0000

    fixing images and dashboard sort of

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,18 @@ Rails.application.routes.draw do
   resource :dashboard, only: [:show]
   root 'static_pages#index'
   get 'privacy', to: 'static_pages#privacy'
+  get 'career', to: 'static_pages#career'
+  get 'team', to: 'static_pages#team'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :courses, only: [:index, :show]
   resources :lessons, only: [:show]
   namespace :instructor do
   resources :lessons, only: [:update]
     resources :sections, only: [:up] do
       resources :lessons, only: [:create]
     end
-    resources :sections, only: []
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create]
     end
